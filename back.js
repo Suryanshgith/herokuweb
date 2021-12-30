6afc297b0ef22f48daeaaa0b23e9f207cac72be6
Suryanshgith <suryanshtechyt@gmail.com>
2021-12-30 12:17:53 +0530

    first err

--- back.js
+++ back.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 const fs = require('fs')
 const home = fs.readFileSync('index.html') 
 
-app.post('', (req, res) => {
+app.get('/', (req, res) => {
   res.end(home)
 })
 app.listen(port, () => { console.log(`Example app listening at http://localhost:${port}`) }) 

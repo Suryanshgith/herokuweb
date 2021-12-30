ca4a05ee221366c0325bb02cd11974259254ea15
Suryanshgith <suryanshtechyt@gmail.com>
2021-12-30 12:02:07 +0530

    Adding backend file

---
+++ back.js
@@ -0,0 +1,11 @@
+const express = require('express') 
+const app = express() 
+const port = process.env.PORT || 3000
+
+const fs = require('fs')
+const home = fs.readFileSync('index.html') 
+
+app.get('/', (req, res) => {
+  res.end(home)
+})
+app.listen(port, () => { console.log(`Example app listening at http://localhost:${port}`) }) 
